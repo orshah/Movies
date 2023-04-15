1cc9163f4ae9c1cce41f0b9ac0f7161d497969c1
orshah <orshpov@gmail.com>
2023-04-15 12:16:35 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+``;
+//Three steps
+//1 step: git add .
+// Optional: git status => checks
+//2 step: => git commit -m : "I will put notes here"
+//3 step => git push
+//
+//git pull => gets all the code in the github.
